24d7ed4b8756b3a9985d4d676fed820c22454b91
Naule Sousa <davenstall@gmail.com>
2023-01-10 21:45:09 -0300

    attack move

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -18,7 +18,7 @@ defmodule ExMon do
   @computer_name "Robotinik"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
-    ExMon.Player.build(name, move_rnd, move_avg, move_heal)
+    Player.build(name, move_rnd, move_avg, move_heal)
   end
 
   def start_game(player) do
---
+++ lib/game/actions/attack.ex
@@ -0,0 +1,5 @@
+defmodule ExMon.Game.Actions.Attack do
+    def attack_opponent(opponent, move) do
+        
+    end
+end
