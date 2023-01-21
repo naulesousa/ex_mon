d9efc6c56c562c59cbbc90d0bf7b46e78a9994db
Naule Sousa <davenstall@gmail.com>
2023-01-20 22:53:47 -0300

    creating heal action

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,6 +1,6 @@
 defmodule ExMon.Game.Actions do
     alias ExMon.Game
-    alias ExMon.Game.Actions.Attack
+    alias ExMon.Game.Actions.{Attack, Heal}
 
     def attack(move) do
       case Game.turn() do
@@ -8,7 +8,14 @@ defmodule ExMon.Game.Actions do
         :computer -> Attack.attack_opponent(:player, move)
       end
     end
-    
+
+    def heal() do
+      case Game.turn() do
+        :player -> Heal.heal_life(:player)
+        :computer -> Heal.heal_life(:computer)
+      end
+    end
+
     def fetch_move(move) do
       Game.player()
       |> Map.get(:moves)
@@ -16,7 +23,7 @@ defmodule ExMon.Game.Actions do
     end
 
     defp find_move(moves, move) do
-        Enum.find_value(moves, {:error, move}, fn {key, value} -> 
+        Enum.find_value(moves, {:error, move}, fn {key, value} ->
            if value == move, do: {:ok, key}
         end)
     end
