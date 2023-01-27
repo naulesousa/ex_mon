fe3641517b1cba507f669da129dea30c4777b290
Naule Sousa <davenstall@gmail.com>
2023-01-26 23:25:43 -0300

    testing game module - info function

--- test/game_test.exs
+++ test/game_test.exs
@@ -5,10 +5,36 @@ defmodule ExMon.GameTest do
 
   describe "start/2" do
     test "starts the game state" do
-      player = Player.build("Naule Sousa", :chute, :soco, :cura)
+      player = Player.build("Soldier P", :chute, :soco, :cura)
       computer = Player.build("Robotinik", :chute, :soco, :cura)
 
       assert {:ok, _pid} = Game.start(computer, player)
+    end
+  end
+
+  describe "info/0" do
+    test "returns the current game state" do
+      player = Player.build("Soldier P", :chute, :soco, :cura)
+      computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+      Game.start(computer, player)
+
+      expected_response = %{
+        computer: %Player{
+          life: 100,
+          moves: %{move_avg: :chute, move_heal: :cura, move_rnd: :soco},
+          name: "Robotinik"
+        },
+        player: %Player{
+          life: 100,
+          moves: %{move_avg: :chute, move_heal: :cura, move_rnd: :soco},
+          name: "Soldier P"
+        },
+        status: :started,
+        turn: :player
+      }
+
+      assert expected_response == Game.info()
     end
   end
 end
