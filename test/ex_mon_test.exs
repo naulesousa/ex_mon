43a6c725ed029b65d9732dc35182079f0db755a2
Naule Sousa <davenstall@gmail.com>
2023-01-28 23:02:41 -0300

    testing ExMon module - create_player function

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,7 +1,16 @@
 defmodule ExMonTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert true == true
+  alias ExMon.Player
+
+  describe "create_player/4" do
+    test "returns a player" do
+      expected_response = %Player{
+        life: 100,
+        moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+        name: "Soldier P"
+      }
+      assert expected_response == ExMon.create_player("Soldier P", :chute, :soco, :cura)
+    end
   end
 end
