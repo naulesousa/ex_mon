850a7992cc29877b465faa44f8df930c116e4b71
Naule Sousa <davenstall@gmail.com>
2023-01-31 19:43:50 -0300

    testing ExMon module - start_game function

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -15,4 +15,19 @@ defmodule ExMonTest do
       assert expected_response == ExMon.create_player("Soldier P", :chute, :soco, :cura)
     end
   end
+
+  describe "start_game/1" do
+    test "when the game is started, returns a message" do
+      player = Player.build("Soldier P", :chute, :soco, :cura)
+
+      messages =
+        capture_io(fn ->
+        assert ExMon.start_game(player) == :ok
+      end)
+
+      assert messages =~ "The game is started!"
+      assert messages =~ "status: :started"
+      assert messages =~ "turn: :player"
+    end
+  end
 end
