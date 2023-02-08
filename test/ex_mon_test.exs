561ebfe581847ae68064825c442e98cb3420bd9c
Naule Sousa <davenstall@gmail.com>
2023-02-08 19:09:31 -0300

    finishing tests

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -32,9 +32,35 @@ defmodule ExMonTest do
   end
 
   describe "make_move/1" do
-    test "when the move is valid, do the move and the computer makes a move" do
+    setup do
       player = Player.build("Soldier P", :chute, :soco, :cura)
-      ExMon.start_game(player)
+
+      capture_io(fn ->
+        ExMon.start_game(player)
+
+        :ok
+      end)
+
+    end
+    test "when the move is valid, do the move and the computer makes a move" do
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:chute)
+      end)
+
+      assert messages =~ "The Player attacked the computer"
+      assert messages =~ "It's computer turn"
+      assert messages =~ "It's player turn"
+      assert messages =~ "status: :continue"
+    end
+
+    test "when the move is invalid, returns an error message" do
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:wrong)
+      end)
+
+      assert messages =~ "Invalid move: wrong"
     end
   end
 end
